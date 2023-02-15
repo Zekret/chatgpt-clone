111f6b6cad01951779c0f4b93eea546753a6a934
Zekret <benjaminconcha5@gmail.com>
2023-02-15 16:19:37 -0300

    update: custom hook useTypingEffect

--- src/components/TypingEffect.jsx
+++ src/components/TypingEffect.jsx
@@ -1,14 +1,14 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 
-export const TypingEffect = ({ text }) => {
+const useTypingEffect = ({ text }) => {
   const [displayText, setDisplayText] = useState('')
-  const [currentIndex, setCurrentIndex] = useState(0)
+  const [currentIndex, setCurrentIndex] = useState(0) // podríamos usar un useRef
   const [showCursor, setShowCursor] = useState(true)
 
   useEffect(() => {
-    if (!text.length) return
+    if (!text?.length) return
 
-    const randomTime = Math.floor(Math.random() * 20) + 5
+    const randomTime = Math.floor(Math.random() * 40) + 15
 
     const intervalId = setInterval(() => {
       if (currentIndex >= text.length) {
@@ -18,7 +18,8 @@ export const TypingEffect = ({ text }) => {
       }
 
       const nextIndex = text.indexOf(' ', currentIndex + 1)
-      if (nextIndex < 0) { // llegar al final del texto
+      if (nextIndex < 0) {
+        // ha llegado al final
         setDisplayText(text)
         setCurrentIndex(text.length)
         return
@@ -27,8 +28,23 @@ export const TypingEffect = ({ text }) => {
       setDisplayText(text.slice(0, nextIndex))
       setCurrentIndex(currentIndex + 1)
     }, randomTime)
+
     return () => clearInterval(intervalId)
   }, [text, currentIndex])
 
-  return <span className={`${showCursor ? 'after:content-["▋"] after:ml-1 after:animate-typing' : ''}`}>{displayText}</span>
+  return { displayText, showCursor }
+}
+
+export function TypingEffect({ text }) {
+  const { displayText, showCursor } = useTypingEffect({ text })
+
+  return (
+    <span
+      className={`${
+        showCursor ? 'after:content-["▋"] after:ml-1 after:animate-typing' : ''
+      }`}
+    >
+      {displayText}
+    </span>
+  )
 }
